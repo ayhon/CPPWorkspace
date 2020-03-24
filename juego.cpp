302b6212c0ce9a887c5624a9ffacddaca3edb753
ayhon <43295942+ayhon@users.noreply.github.com>
2020-03-24 20:33:04 +0100

    Add windows-ready project files
    Estos son los archivos del proyecto que fucnionan bien en Windows 10. Falta la parte de Linux.

---
+++ juego.cpp
@@ -0,0 +1,296 @@
+//Author: Ayhon
+#include <iostream>
+#include <climits>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "juego.h"
+#ifdef _WIN32
+#include <conio.h>
+#elif __linux__
+#include <ncurses.h>
+#endif
+#ifdef DOMJUDGE
+const bool DEBUG = false;
+#else
+const bool DEBUG = false;
+#endif
+
+using namespace std;
+
+void Log(string const& msg){ 
+	if (DEBUG) cout << msg << '\n';
+}
+
+void explosion(tJuego& juego, int x, int y) {
+	tPlano& plano = juego.mina.plano;
+	for (int i = x-1; i <= x+1; i++) {
+		for (int j = y-1; j <= y+1; j++) {
+			if(dentroPlano(juego.mina, i, j)){
+				if(plano[i][j] == MINERO) juego.estado = EXPLOTADO;
+				plano[i][j] = LIBRE;
+			} 
+		}
+	}
+	for (size_t j = y-1; j <= y+1; j++) {
+		if(dentroPlano(juego.mina, x-2, j)) avalancha(juego, x-2, j);
+	}
+	
+}
+
+void lanzarDinamita(tJuego& juego) {
+	juego.dinamitasUsadas++;
+	int& x = juego.mina.minero.x,
+		y = juego.mina.minero.y;
+	tPlano& plano = juego.mina.plano;
+	int tx; // target x
+	for (tx = x + 1; tx < plano.size() && plano[tx][y] == LIBRE; tx++) {
+		tElemento tmp = plano[tx][y];
+		plano[tx][y] = DINAMITA;
+		dibujar(juego);
+		plano[tx][y] = tmp;
+	}
+	tx--; // Volvamos a la última posición válida
+	explosion(juego, tx, y);
+}
+
+void moverMinero(tJuego& juego, int hasta_x, int hasta_y) {
+	int& desde_x = juego.mina.minero.x,
+		 desde_y = juego.mina.minero.y;
+	tPlano& plano = juego.mina.plano;
+	plano[hasta_x][hasta_y] = plano[desde_x][desde_y];
+	plano[desde_x][desde_y] = LIBRE;
+	juego.mina.minero.x = hasta_x;
+	juego.mina.minero.y = hasta_y;
+}
+
+void queCaiga(tJuego & juego, int x, int y) {
+	// !! No se encarga de que (x,y) deba caer
+	tPlano& plano = juego.mina.plano;
+	if(dentroPlano(juego.mina, x+1, y) && plano[x+1][y] == LIBRE) {
+			int nx;
+			for(nx = x+1; nx < plano.size() && plano[nx][y] == LIBRE; ++nx);
+			swap(plano[x][y], plano[nx-1][y]);
+	}
+}
+
+void avalancha(tJuego & juego, int x, int y) {
+	int tx;
+	for(tx = x; tx >= 0 && esElemQueCae(juego.mina.plano[tx][y]); --tx) {
+		queCaiga(juego, tx, y);
+	}
+}
+
+void printStats(tJuego const& juego) {
+	cout << "Movimientos: " << juego.numMovimientosMinero << '\n';
+	cout << "Gemas: " << juego.gemasRecogidas << '\n';
+	cout << "Dinamitas: " << juego.dinamitasUsadas << '\n';
+}
+
+istream& operator>>(istream& movimientos, tTecla& tecla) {
+	char input;
+	movimientos.get(input);
+	switch(input){
+		case 'A':
+			tecla = ARRIBA;
+			break;
+		case 'Z':
+			tecla = ABAJO;
+			break;
+		case 'M':
+			tecla = DCHA;
+			break;
+		case 'N':
+			tecla = IZDA;
+			break;
+		case 'S':
+			tecla = SALIR;
+			break;
+		case 'D':
+			tecla = TNT;
+			break;
+		case '\n':
+			tecla = FIN;
+			break;
+		default:
+			tecla = ERROR_TECLA;
+	}
+	return movimientos;
+}
+
+tTecla leerTeclado() {
+	tTecla tecla = ERROR_TECLA;
+	#ifdef _WIN32
+		int dir = _getch();
+		if (dir = 0xe0) 
+			dir = _getch();
+		switch(dir) {
+			case 27:
+				tecla = SALIR;
+				break;
+			case 72:
+				tecla = ARRIBA;
+				break;
+			case 80:
+				tecla = ABAJO;
+				break;
+			case 77:
+				tecla = DCHA;
+				break;
+			case 75:
+				tecla = IZDA;
+				break;
+			case 32:
+			case 64:
+			case 44:
+				tecla = TNT;
+				break;
+		}
+		return tecla;
+	#elif __linux__
+	initscr();
+	raw();
+	keypad(stdscr, TRUE);
+	noecho();
+
+	int dir = getchar();
+	if(dir = 27) dir = getchar();
+	if(dir = 91) dir = getchar();
+	else dir = 27;
+	switch(dir) {
+	case 65:
+		tecla = ARRIBA;
+		break;
+	case 66:
+		tecla = ABAJO;
+		break;
+	case 67:
+		tecla = DCHA;
+		break;
+	case 68:
+		tecla = IZDA;
+		break;
+	case 27:
+		tecla = SALIR;
+		break;
+	case 32:
+	case 64:
+	case 44:
+		tecla = TNT;
+		break;
+	}
+
+	endwin();
+	return tecla;
+	#endif
+}
+
+void leerMovimiento(tJuego & juego, tTecla & tecla, istream & movimientos) {
+	if(juego.dispositivoDeEntrada == 1)
+		tecla = leerTeclado();
+	else if(juego.dispositivoDeEntrada == 2)
+		movimientos >> tecla;
+
+	if (tecla == SALIR)
+		juego.estado = ABANDONO;
+}
+
+void dibujar(tJuego const& juego) {
+#ifdef _WIN32
+	system("cls");
+#elif __linux__
+	system("clear");
+#endif
+	printStats(juego);
+	switch(juego.resolucion) {
+		case 1:
+			dibujar1_1(juego.mina);
+			break;
+		case 2:
+			dibujar3_1(juego.mina);
+			break;
+		default:
+			Log("No se acepta la resolución " + to_string(juego.resolucion) + "\n");
+	}
+	if(juego.estado == EXITO) cout << "Mina recorrida\n";
+	else if(juego.estado == EXPLOTADO) cout << "Le alcanzo la dinamita\n";
+	else cout << "Pide rescate\n";
+}
+
+void realizarMovimiento(tJuego& juego, tTecla& tecla) {
+	juego.numMovimientosMinero++;
+	tPlano& plano = juego.mina.plano;
+	int x = juego.mina.minero.x,
+		y = juego.mina.minero.y;
+	int nx = x, ny = y;
+
+	nx += tdirs4[int(tecla)].x;
+	ny += tdirs4[int(tecla)].y;
+	bool hazMovim = false;
+	if(int(tecla) < 4) {
+		if(dentroPlano(juego.mina, nx, ny)) {
+			switch(plano[nx][ny]) {
+				case MURO:
+					Log("Te chocaste contra un muro");
+					break;
+				case PIEDRA:
+					if(tecla == DCHA || tecla == IZDA) {
+						int x_detras = nx + tdirs4[int(tecla)].x,
+							y_detras = ny + tdirs4[int(tecla)].y;
+						// Mover la piedra si hay espacio
+						if(dentroPlano(juego.mina, x_detras, y_detras) && plano[x_detras][y_detras] == LIBRE) {
+							swap(plano[x_detras][y_detras], plano[nx][ny]);
+							hazMovim = true;
+							// Que caiga la piedra si tiene que caer
+							avalancha(juego, x_detras, y_detras);
+						}
+						else Log("No hay un espacio libre detras de esa piedra");
+					}
+					break;
+				case TIERRA:
+				case LIBRE:
+					hazMovim = true;
+					break;
+				case GEMA:
+					hazMovim = true;
+					++juego.gemasRecogidas;
+					break;
+				case SALIDA:
+					hazMovim = true;
+					juego.estado = EXITO;
+					break;
+				default:
+					Log("No se pudo detectar el elemento");
+			}
+			if (hazMovim) {
+				moverMinero(juego, nx, ny); //Actualiza juego.mina.minero pero no x o y
+				if (x != 0 && esElemQueCae(plano[x-1][y]))
+					avalancha(juego, x-1, y);
+			}
+			else Log("No te mueves este turno");
+		}
+	}
+	else if (tecla == TNT) {
+		lanzarDinamita(juego);
+	}
+	else Log("Has realizado un movimiento indebido, hacia ("+to_string(nx)+"-"+to_string(ny)+")\n");
+	dibujar(juego);
+}
+
+
+void jugar(tJuego & juego, istream & entrada, istream & movimientos) {
+	cargar_mina(entrada, juego.mina);
+	dibujar(juego);
+	tTecla input;
+	do{
+		leerMovimiento(juego, input, movimientos);
+		if(juego.estado != ABANDONO && input != FIN)  {
+			realizarMovimiento(juego, input);
+		} 
+	} while(input != FIN && juego.estado == EXPLORANDO);
+	dibujar(juego);
+	/*
+	if(juego.dispositivoDeEntrada = 2) 
+		while(input != FIN) leerMovimiento(juego, input, movimientos); // Agotar movimientos no usados
+	*/
+}
---
+++ sprites.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <string>
+#include "sprites.h"
+using namespace std;
+const vector<int> ctr = { 30,34,32,36,31,35,33,37,90 };
+
+void printTitleScreen(tColor colorFondo, tColor colorTexto) {
+	system("cls");
+    colorear(colorFondo, "                                                                                                 \n", colorTexto);
+    colorear(colorFondo, " ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗     ███╗   ███╗██╗███╗   ██╗███████╗██████╗ \n", colorTexto);
+	colorear(colorFondo, " ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗    ████╗ ████║██║████╗  ██║██╔════╝██╔══██╗\n", colorTexto);
+	colorear(colorFondo, " ██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝    ██╔████╔██║██║██╔██╗ ██║█████╗  ██████╔╝\n", colorTexto);
+	colorear(colorFondo, " ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗    ██║╚██╔╝██║██║██║╚██╗██║██╔══╝  ██╔══██╗\n", colorTexto);
+	colorear(colorFondo, " ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║    ██║ ╚═╝ ██║██║██║ ╚████║███████╗██║  ██║\n", colorTexto);
+	colorear(colorFondo, " ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝    ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝\n", colorTexto);
+}
+
+void printGameOver(tColor colorFondo, tColor colorTexto) {
+	system("cls");
+    colorear(colorFondo, "                                     \n", colorTexto);
+	colorear(colorFondo, "   ▄████  ▄▄▄       ███▄ ▄███▓▓█████ \n", colorTexto);
+	colorear(colorFondo, "  ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀ \n", colorTexto);
+	colorear(colorFondo, " ▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███   \n", colorTexto);
+	colorear(colorFondo, " ░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄ \n", colorTexto);
+	colorear(colorFondo, " ░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒\n", colorTexto);
+	colorear(colorFondo, "  ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░\n", colorTexto);
+	colorear(colorFondo, "   ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░\n", colorTexto);
+	colorear(colorFondo, " ░ ░   ░   ░   ▒   ░      ░      ░   \n", colorTexto);
+	colorear(colorFondo, "       ░       ░  ░       ░      ░  ░\n", colorTexto);
+	colorear(colorFondo, "                                     \n", colorTexto);
+	colorear(colorFondo, "  ▒█████   ██▒   █▓▓█████  ██▀███    \n", colorTexto);
+	colorear(colorFondo, " ▒██▒  ██▒▓██░   █▒▓█   ▀ ▓██ ▒ ██▒  \n", colorTexto);
+	colorear(colorFondo, " ▒██░  ██▒ ▓██  █▒░▒███   ▓██ ░▄█ ▒  \n", colorTexto);
+	colorear(colorFondo, " ▒██   ██░  ▒██ █░░▒▓█  ▄ ▒██▀▀█▄    \n", colorTexto);
+	colorear(colorFondo, " ░ ████▓▒░   ▒▀█░  ░▒████▒░██▓ ▒██▒  \n", colorTexto);
+	colorear(colorFondo, " ░ ▒░▒░▒░    ░ ▐░  ░░ ▒░ ░░ ▒▓ ░▒▓░  \n", colorTexto);
+	colorear(colorFondo, "   ░ ▒ ▒░    ░ ░░   ░ ░  ░  ░▒ ░ ▒░  \n", colorTexto);
+	colorear(colorFondo, " ░ ░ ░ ▒       ░░     ░     ░░   ░   \n", colorTexto);
+	colorear(colorFondo, "     ░ ░        ░     ░  ░   ░       \n", colorTexto);
+	colorear(colorFondo, "               ░                     \n", colorTexto);
+}
+
+void printVictory(tColor colorFondo, tColor colorTexto) {
+	system("cls");
+	colorear(NEGRO, "                                                                                 \n", colorTexto);
+	colorear(NEGRO, "  ▄█    █▄   ▄█   ▄████████     ███      ▄██████▄     ▄████████  ▄█     ▄████████\n", colorTexto);
+	colorear(NEGRO, " ███    ███ ███  ███    ███ ▀█████████▄ ███    ███   ███    ███ ███    ███    ███\n", colorTexto);
+	colorear(NEGRO, " ███    ███ ███▌ ███    █▀     ▀███▀▀██ ███    ███   ███    ███ ███▌   ███    ███\n", colorTexto);
+	colorear(NEGRO, " ███    ███ ███▌ ███            ███   ▀ ███    ███  ▄███▄▄▄▄██▀ ███▌   ███    ███\n", colorTexto);
+	colorear(NEGRO, " ███    ███ ███▌ ███            ███     ███    ███ ▀▀███▀▀▀▀▀   ███▌ ▀███████████\n", colorTexto);
+	colorear(NEGRO, " ███    ███ ███  ███    █▄      ███     ███    ███ ▀███████████ ███    ███    ███\n", colorTexto);
+	colorear(NEGRO, " ███    ███ ███  ███    ███     ███     ███    ███   ███    ███ ███    ███    ███\n", colorTexto);
+	colorear(NEGRO, "  ▀██████▀  █▀   ████████▀     ▄████▀    ▀██████▀    ███    ███ █▀     ███    █▀ \n", colorTexto);
+	colorear(NEGRO, "                                                     ███    ███                  \n", colorTexto);
+
+
+}
+
+void sprite(tElemento elem, int section, tColor colorFondo, tColor colorTexto) {
+    switch(elem) {
+        case MINERO:
+            if(section == 1) {
+				colorear(colorFondo, "┏━┓", colorTexto);
+            }
+            else if (section == 2) {
+				colorear(colorFondo, "┃w┃", colorTexto);
+            }
+            else if (section == 3) {
+				colorear(colorFondo, "┣━┫", colorTexto);
+            }
+			break;
+		case DINAMITA:
+            if(section == 1) {
+				colorear(colorFondo, "╔═╗", colorTexto);
+            }
+            else if (section == 2) {
+				colorear(colorFondo, "TNT", colorTexto);
+			}
+            else if (section == 3) {
+				colorear(colorFondo, "╚═╝", colorTexto);
+            }
+			break;
+		case GEMA:
+            if(section == 1) {
+				colorear(colorFondo, "╭─╮", colorTexto);
+            }
+            else if (section == 2) {
+				colorear(colorFondo, "│€│", colorTexto);
+			}
+            else if (section == 3) {
+				colorear(colorFondo, "╰─╯", colorTexto);
+            }
+			break;
+		case SALIDA:
+            if(section == 1) {
+				colorear(colorFondo, "┎─┐", colorTexto);
+            }
+            else if (section == 2) {
+				colorear(colorFondo, "┠─┘", colorTexto);
+			}
+            else if (section == 3) {
+				colorear(colorFondo, "┃  ", colorTexto);
+            }
+			break;
+		default:
+			string msg = "";
+			for (size_t i = 0; i < 3; i++) {
+				msg += to_string(elem);
+			}
+			colorear(colorFondo, msg, colorTexto);
+    }
+}
+
+void colorear(tColor colorFondo, string msg, tColor colorTexto) {
+    #ifdef _WIN32
+        HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(handle, int(colorTexto) | (int(colorFondo) << 4));
+        cout << msg;
+        SetConsoleTextAttribute(handle, 15 | (0 << 4));
+    #elif __linux__
+        cout << "\e[1;" << ctr[int(colorTexto)] << ";" << ctr[int(colorFondo)] + 10 << "m" + msg + "\e[m";
+    #endif
+}
