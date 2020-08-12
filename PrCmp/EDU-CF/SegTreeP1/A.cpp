a478af08e18abaaf7c2383ef08a5a62d304a0c58
Ayhon <ayhonnick@gmail.com>
2020-08-12 11:40:52 +0200

    Codeforces EDU courses files and such

---
+++ PrCmp/EDU-CF/SegTreeP1/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void resuelveCaso() {
+	
+}
+
+int main() {
+	int T; cin >> T;
+	for (int i = 1; i <= T; i++) {
+		//cout << "Case #" << i << ": ";
+		resuelveCaso();
+	}
+	return 0;
+}
+
+
